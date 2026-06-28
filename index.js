6aef2a58b2127eefa57b021870e6e5f6952ddd4a
paalakash <palaksh452@gmail.com>
2026-06-28 10:27:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://frtgfiujfdgfg.on-forge.com", weight: 1.0 }
+    { url: "https://clownfish-app-teuy8.ondigitalocean.app/", weight: 1.0 }
 
 ];
 
