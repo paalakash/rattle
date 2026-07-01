c4e6004071e79d2dd7b415301ceb35a08e503019
paalakash <palaksh452@gmail.com>
2026-07-01 09:57:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seal-app-kevmi.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://536d4f94-16fe-430e-9cdf-27ef21be00fb.svc.edge.scw.cloud/index.html", weight: 1.0 }
 
 ];
 
